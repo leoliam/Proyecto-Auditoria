c61cf076e96b804254a23afbd79b42b2481c69aa
leoliam <liamcaleb.asr@gmail.com>
2014-12-16 01:10:02 -0500

    heroku1

--- proyecto/settings/staging.py
+++ proyecto/settings/staging.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'consultorio',
-        'USER': 'root',
-        'PASSWORD': 'dynamo',
+        'ENGINE': 'ec2-54-235-193-41.compute-1.amazonaws.com',
+        'NAME': 'd5tn8valh32ctg',
+        'USER': 'aqmmhbacbbpxxw',
+        'PASSWORD': 'A8rimTOao7gndx_lYClMXIPfbC',
         'HOST' : 'ec2-54-163-255-191.compute-1.amazonaws.com',
         'PORT' : '5432',
     }
